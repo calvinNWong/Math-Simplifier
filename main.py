da1c45b9077090fe425e5487e13365783e4d41a2
calvinNWong <127069297+calvinNWong@users.noreply.github.com>
2023-11-08 21:56:42 +0000

    Added find numbers function and tried to make a check around operators function.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 operators = []
 operatorsPosition = []
+
+originalEquation = "y=5(x-5.1)^2+9"
 solveFor = "y"
 
-
+#Find operators in the equation
 def findOperators(equation):
   repeats = 0
   for char in equation:
@@ -24,25 +26,50 @@ def findOperators(equation):
       print("found parenthesis")
     repeats += 1
 
+#Find numbers in the equation
+def findNumbers(equation):
+  for char in equation:
+    if char.isnumeric:
+      print("found number " + char)
+
 
 def checkAroundOperators(equation):
   repeats = 0
   for char in operators:
     #if operator is +
     if char == "+":
-      if 
-      if equation[operatorsPosition[repeats] - 1].isnumeric == False and equation[operatorsPosition[repeats] - 1] != solveFor or equation[operatorsPosition[repeats] + 1].isnumeric == False and equation[operatorsPosition[repeats] + 1] != solveFor:
-        print("Nothing to do")
-      elif equation[operatorsPosition[repeats] - 1].isnumeric and equation[operatorsPosition[repeats] + 1].isnumeric:
-        print("adding two numbers")
-      elif equation[operatorsPosition[repeats] - 1] == equation[operatorsPosition[repeats] + 1]:
-        print("Replace with 2", equation[operatorsPosition[repeats] - 1])
+      #if operator is next to 2 numeric numbers
+      if equation[operatorsPosition[repeats] -
+                  1].isnumeric == True and equation[operatorsPosition[repeats] + 1].isnumeric == True:
+        print("Adding two numbers")
+      #else if the operator is next to a variable
+      elif equation[operatorsPosition[repeats] - 1].isnumeric == False or equation[operatorsPosition[repeats] + 1].isnumeric == False:
+        #if the two variables are the same
+        if equation[operatorsPosition[repeats] - 1] == equation[operatorsPosition[repeats] + 1]:
+          print("Simplified two variables to 2(variable)")
+        elif equation[operatorsPosition[repeats] - 1] == solveFor or equation[operatorsPosition[repeats] + 1] == solveFor:
+          print("Do nothing")
+        elif equation[operatorsPosition[repeats] - 1] != solveFor and equation[operatorsPosition[repeats] + 1] != solveFor:
+          print("Do nothing")
+        elif equation[operatorsPosition[repeats] - 1] == solveFor and equation[operatorsPosition[repeats] + 1] == solveFor:
+          print("Subtracting both sides by (not solveFor)")
     #else if operator is -
     elif char == "-":
-      if equation[operatorsPosition[repeats] - 1].isnumeric == False and equation[operatorsPosition[repeats] - 1] != solveFor or equation[operatorsPosition[repeats] + 1].isnumeric == False and equation[operatorsPosition[repeats] + 1] != solveFor:
-        print("Nothing to do")
-      elif 
+      if equation[operatorsPosition[repeats] -
+                  1].isnumeric == True and equation[operatorsPosition[repeats]
+                                                    + 1].isnumeric == True:
+        print("Subtracting two numbers")
+      elif equation[operatorsPosition[repeats] -
+                    1].isnumeric == False or equation[
+                        operatorsPosition[repeats] + 1].isnumeric == False:
+        if equation[operatorsPosition[repeats] -
+                    1] != solveFor or equation[operatorsPosition[repeats] +
+                                               1] != solveFor:
+          print("Nothing to do")
 
     repeats += 1
 
-findOperators("y=m(x-k)^z+h")
+
+findOperators(originalEquation)
+findNumbers(originalEquation)
+checkAroundOperators(originalEquation)
